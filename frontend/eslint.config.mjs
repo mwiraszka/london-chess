87538ba7543a1e4f3e9bdc8559e7fab6e1274f8e
mwiraszka <michal@wiraszka.com>
2026-09-14 12:55:36 +0200

    chore: add eslint rule to sort component imports

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,4 +1,5 @@
 import pluginJs from '@eslint/js';
+import perfectionist from 'eslint-plugin-perfectionist';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -9,8 +10,18 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    plugins: { perfectionist },
     rules: {
       '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true }],
+      'perfectionist/sort-arrays': [
+        'error',
+        {
+          useConfigurationIf: {
+            matchesAstSelector:
+              "Decorator > CallExpression[callee.name='Component'] > ObjectExpression > Property[key.name='imports'] > ArrayExpression",
+          },
+        },
+      ],
     },
   },
 ];
