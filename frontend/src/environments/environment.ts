c3a562ab3cc36c4cadecc57c9fdc6e7336d1acfc
mwiraszka <michal@wiraszka.com>
2026-09-15 23:54:40 +0200

    chore: report local development errors to Sentry

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   clerkPublishableKey: 'pk_test_bmVlZGVkLWJhcm5hY2xlLTU0LmNsZXJrLmFjY291bnRzLmRldiQ',
   googleMapsApiKey: 'AIzaSyCeuTgq4qe2k8obnbYWrwWsG91B6aDUSA0',
   lccApiBaseUrl: 'http://localhost:3000/v1',
-  sentryDsn: '',
+  sentryDsn:
+    'https://15ca529d0b2ea8ff491edc33e2f234fc@o4511323014234112.ingest.de.sentry.io/4511323126956112',
   isPreview: false,
 };
